67589cfd599afee0af28b929334a29dcca6e35b1
PokumeKachi <108186213+PokumeKachi@users.noreply.github.com>
2026-07-26 21:27:01 +0700

    feat: 2244C attempt

--- codeforces/2244C.cpp
+++ codeforces/2244C.cpp
@@ -6,6 +6,33 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
+    int t;
+
+    cin >> t;
+
+    string output = "";
+
+    for (int i = 0; i < t; ++i) {
+        int n, x, y;
+
+        cin >> n >> x >> y;
+
+        std::array<int, 200005> p;
+
+        for (int j = 0; j < n; ++j) {
+            int input;
+            cin >> input;
+            p[j] = input;
+        }
+
+        if (is_sorted(p.begin(), p.begin() + n) || ((x & 1) ^ (y & 1)) || (x == 1 || y == 1)) {
+            output.append("YES\n");
+        } else {
+            output.append("NO\n");
+        }
+    }
+
+    cout << output;
 
     return 0;
 }
